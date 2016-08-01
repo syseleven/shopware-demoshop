5602b24d4fc35616676169233c4ac8046622e109
Sascha Doering <s.doering@syseleven.de>
2016-08-01 13:58:09 +0200

    add config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php return array (
+  'db' =>
+  array (
+    'host' => '<|VAR::DATABASEADDRESS|>',
+    'port' => '3306',
+    'username' => 'shopware',
+    'password' => '<|VAR::DATABASEPASSWORD|>',
+    'dbname' => 'shopware',
+  ),
+);
+
